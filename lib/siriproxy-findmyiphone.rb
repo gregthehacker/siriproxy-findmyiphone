868e20c43ec7ba3b0f674a0b9251028c372dff11
Matthew Bowman <mgbowman@gmx.com>
2013-02-04 21:41:55 +0100

    Added more log statements for debugging

--- lib/siriproxy-findmyiphone.rb
+++ lib/siriproxy-findmyiphone.rb
@@ -23,13 +23,14 @@ class SiriProxy::Plugin::FindMyIPhone < SiriProxy::Plugin
     end
   end
 
-  listen_for /where(?:'s| is) (.* (?:iphone|ipad))/i do |iphone_name|
+  listen_for "/where(?:'s| is) (.* (?:iphone|ipad))/i" do |iphone_name|
     device_name = scrub(iphone_name)
     device_name = @aliases[device_name] || device_name
     auth = @credentials[device_name]
     # mgb: have siri say 'your iphone' when you ask about 'my iphone' :)
     iphone_name = iphone_name.gsub('my', 'your')
     if auth
+      log "Logging in with username = '#{auth['username']}'"
       say @wait_msg % iphone_name
       Thread.new {
         begin
@@ -40,7 +41,7 @@ class SiriProxy::Plugin::FindMyIPhone < SiriProxy::Plugin
             add_views = nil
             if device_num
               loc = fmi.locate(device_num, 60)
-              log "Found Device ##{device_num} = #{fmi.devices[device_num]['name']} @ #{loc[:latitude]},#{loc[:longitude]}"
+              log "Found device ##{device_num} = #{fmi.devices[device_num]['name']} @ #{loc[:latitude]},#{loc[:longitude]}"
               #def initialize(label="Apple", street="1 Infinite Loop", city="Cupertino", stateCode="CA", countryCode="US", postalCode="95014", latitude=37.3317031860352, longitude=-122.030089795589)
               add_views = SiriAddViews.new
               add_views.make_root(last_ref_id)
@@ -50,7 +51,9 @@ class SiriProxy::Plugin::FindMyIPhone < SiriProxy::Plugin
               map_snippet.items[0].location = SiriLocation.new(loc[:name], "", "", "", "", "", loc[:latitude], loc[:longitude])
               utterance = SiriAssistantUtteranceView.new(@ok_msg % iphone_name)
               add_views.views << utterance
-              add_views.views << map_snippet              
+              add_views.views << map_snippet  
+            else
+              log "Could not find device # for '#{iphone_name}'"            
             end
             if add_views
               send_object add_views
@@ -65,18 +68,20 @@ class SiriProxy::Plugin::FindMyIPhone < SiriProxy::Plugin
         end
       }
     else
+      log "No auth for '#{iphone_name}'"
       say @err_msg % iphone_name
       request_completed
     end
   end
 
-  listen_for /find (.* (?:iphone|ipad))/i do |iphone_name|
+  listen_for "/find (.* (?:iphone|ipad))/i" do |iphone_name|
     device_name = scrub(iphone_name)
     device_name = @aliases[device_name] || device_name
     auth = @credentials[device_name]
     # mgb: have siri say 'your iphone' when you ask about 'my iphone' :)
     iphone_name = iphone_name.gsub('my', 'your')
     if auth
+      log "Logging in with username = '#{auth['username']}'"
       say @wait_msg % iphone_name
       Thread.new {
         begin
@@ -85,11 +90,13 @@ class SiriProxy::Plugin::FindMyIPhone < SiriProxy::Plugin
             device_num = nil            
             fmi.devices.each_with_index { |device, num| device_num = num if scrub(device['name']) == device_name }
             if device_num
+              log "Found device ##{device_num} = #{device['name']}"
               device = fmi.devices[device_num]
-              log "Found Device ##{device_num} = #{device['name']}"
               # mgb: change the subject based on device
               type = device_name =~ /iphone$/ ? 'iPhone' : 'iPad'
               fmi.sendMessage(device_num, "Find My #{type} Alert", @text)
+            else
+              log "Could not find device # for '#{iphone_name}'"
             end
             say device_num ? @ok_msg % iphone_name : @err_msg % iphone_name 
             request_completed
@@ -100,6 +107,7 @@ class SiriProxy::Plugin::FindMyIPhone < SiriProxy::Plugin
         end
       }
     else
+      log "No auth for '#{iphone_name}'"
       say @err_msg % iphone_name
       request_completed
     end
